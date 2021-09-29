23ea7ada36751cecee097ba2b67e04311811f916
Gamef <ifeanyi20162016@outlook.com>
2021-09-29 01:56:05 +0100

    added logo and updated titles

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div class='mt-20'>
       <Logo style='width:120px;margin:auto' />
       <h1 class='text-center text-3xl mt-10'>
-        NuxtJS KickStart Template
+       Gamefs KickStart Template
       </h1>
       <h2 class='text-center'>
-        Create simple, beautiful, and elegant website.
+        Create simple, dope, and elegant website.
       </h2>
       
     </div>
